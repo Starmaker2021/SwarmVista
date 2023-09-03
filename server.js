73775a16cbf64556f927f78efe57df8dd1fd218b
Starmaker2021 <2968722911@qq.com>
2023-09-03 13:51:54 +0100

    Express Config Modification

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
-
+// 设置静态文件的MIME类型
+app.use(express.static(path.join(__dirname), {
+  setHeaders: function (res, pathToFile) {
+    if (pathToFile.endsWith('.js')) {
+      res.setHeader('Content-Type', 'text/javascript');
+    }
+  }
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'cover.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
